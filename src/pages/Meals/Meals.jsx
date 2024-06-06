21de3e723825e5707fce3bac067eec887045a48c
Md Ataullah <ataullahm100@gmail.com>
2024-06-06 23:23:09 +0600

    Infinite Scroll

--- src/pages/Meals/Meals.jsx
+++ src/pages/Meals/Meals.jsx
@@ -1,13 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import useAxiosPub from '../../Hooks/useAxiosPub';
 import MealCard from './MealCard';
+import { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Meals = () => {
   const axiosPub = useAxiosPub();
+  const [page, setPage] = useState(1);
+  const [itemper, setItemper] = useState(3);
   const { data: meals = [] } = useQuery({
     queryKey: ['meals'],
     queryFn: async () => {
-      const { data } = await axiosPub.get(`/meals`);
+      const { data } = await axiosPub.get(
+        `/meals?page=${page}&itemper=${itemper}`
+      );
       return data;
     },
   });
@@ -15,11 +21,27 @@ const Meals = () => {
     <div>
       <div className="h-60 bg-yellow-500"></div>
       <div className="w-11/12 xl:w-10/12 mx-auto">
-        <div className="py-8 bg-slate-500 mt-6 rounded-t-md"></div>
+        <div className="py-8 bg-slate-500 mt-6 rounded-t-md">
+          <h1 className="px-5 text-2xl">{meals?.length}</h1>
+        </div>
         <div className="mb-16 mt-5 flex flex-col gap-5">
-          {meals.map((dta) => (
+          {/* {meals.map((dta) => (
             <MealCard key={dta._id} dta={dta} />
-          ))}
+          ))} */}
+
+          <InfiniteScroll
+            dataLength={this.state.items.length}
+            next={this.fetchMoreData}
+            style={{ display: 'flex', flexDirection: 'column-reverse' }} //To put endMessage and loader to the top.
+            inverse={true} //
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            scrollableTarget="scrollableDiv"
+          >
+            {meals.state.items.map((dta) => (
+              <MealCard key={dta._id} dta={dta} />
+            ))}
+          </InfiniteScroll>
         </div>
       </div>
     </div>
