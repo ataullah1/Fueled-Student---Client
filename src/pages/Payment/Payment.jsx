f73835fe97aa19edcfc4af63ea90f2e5d6de5742
Md Ataullah <ataullahm100@gmail.com>
2024-06-12 03:19:18 +0600

    Payment route update "

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -17,16 +17,26 @@ const Payment = () => {
   }
   return (
     <div>
-      <div className="h-72 w-full bg-pClr"></div>
-      <div className="w-11/12 xl:w-10/12 mx-auto max-w-[1700px]">
-        <h1 className="text-3xl text-slate-100 font-bold text-center pt-12 pb-4">
-          Pay for the {id} Plan
-        </h1>
-        <div className="max-w-[550px] mx-auto pt-10 px-3 rounded-md border-slate-400 border-2 mb-10">
-          <Elements stripe={stripePromise} className="">
-            <ChecoutFrom price={price} badge={id} />
-          </Elements>
-        </div>
+      <div
+        className="h-60 bg-pClr"
+        style={{
+          backgroundImage: `url('https://i.ibb.co/09rvF5m/ffd.png')`,
+        }}
+      ></div>
+      <div className="w-11/12 xl:w-10/12 mx-auto max-w-[1700px] flex flex-col md:flex-row gap-5">
+       <div className='w-full md:w-1/2'>
+
+       </div>
+       <div className='w-full md:w-1/2'>
+          <h1 className="text-3xl text-slate-100 font-bold text-center pt-12 pb-4">
+            Pay for the {id} Plan
+          </h1>
+          <div className="max-w-[550px] mx-auto pt-10 px-3 rounded-md border-slate-400 border-2 mb-10">
+            <Elements stripe={stripePromise} className="">
+              <ChecoutFrom price={price} badge={id} />
+            </Elements>
+          </div>
+       </div>
       </div>
     </div>
   );
